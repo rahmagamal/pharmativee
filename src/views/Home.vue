1d35c776d0df1e964fb7bb74a5d0d4d645f86135
rahmagamal <55654504+rahmagamal@users.noreply.github.com>
2020-07-26 06:52:09 +0200

    title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
     <Header />
-    <div class="content">
-     <hooper :itemsToShow="1" :centerMode="true"  :autoPlay="true">
-          <slide>
-           <img width="100%" src="../assets/pic1.jpg">
+    <div class="">
+     <hooper :itemsToShow="1" :centerMode="true"  :autoPlay="false">
+          <slide class="background">
+           <img width="100%" src="../assets/pic22.jpg">
+           <div class="contentt">
+               <h1 class="title"><strong class="textColor">Pharmative</strong> Opens 24 Hours</h1>
+              <h4 class="subTitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br /> Facilis ex perspiciatis non quibusdam vel quidem.</h4>
+             <button type="button" class="btn  bttn">Shop Now</button>
+           </div>
+          
           </slide>
-          <slide>
-            <img width="100%" src="../assets/pic2.jpg">
+          <slide class="background">
+            <img width="100%" src="../assets/pic1.jpg">
+           
+             <div class="contentt">
+               <h1 class="title"> New Medicine <strong class="textColor">Everday</strong> </h1>
+              <h4 class="subTitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br /> Facilis ex perspiciatis non quibusdam vel quidem.</h4>
+             <button type="button" class="btn  bttn">Shop Now</button>
+           </div>
           </slide>
-          <slide>
-             <img width="100%" src="../assets/pic3.jpg">
+           <slide class="background">
+            <img width="100%" src="../assets/pic33.jpg">
+           
+             <div class="contentt">
+               <h1 class="title textColor"> New Medicine <strong class="textColor">Everday</strong> </h1>
+              <h4 class="subTitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br /> Facilis ex perspiciatis non quibusdam vel quidem.</h4>
+             <button type="button" class="btn  bttn">Shop Now</button>
+           </div>
           </slide>
    <hooper-pagination slot="hooper-addons"></hooper-pagination>
  
@@ -53,5 +71,46 @@ export default {
 .hooper {
     
     height: fit-content !important;
+}
+.contentt
+{
+ position: absolute;
+  top:30%;
+  left: 34%
+  
+}
+.title{
+  color: white;
+}
+.subTitle{
+
+  color: white;
+}
+.bttn{
+    font-size: 25px;
+    background-color: white;
+    border-color: white;
+    color:#75b239;
+}
+.bttn :hover{
+   border-color: #fff !important;
+   background-color: red !important;
+    color:#75b239 !important;
+}
+.background{
+  position :relative
+}
+.textColor{
+  color: #75b239 ;
+}
+
+@media screen and (max-width:763px) {
+  .contentt
+{
+ position: absolute;
+  top: 28% !important;
+    left: 0% !important;
+  
+}
 }
 </style>
